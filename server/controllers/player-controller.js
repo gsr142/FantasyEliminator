1e6717ab16f3da9eaf1b2519985baa95555f4fd8
Greg Richardson <gsr142@gmail.com>
2024-03-11 13:21:29 -0700

    add dotenv and api call to populate playerDB

--- server/controllers/player-controller.js
+++ server/controllers/player-controller.js
@@ -1,22 +1,46 @@
 // import player model
-const { Player } = require('../models');
+const { Player } = require("../models");
 
 module.exports = {
-    // get player(s) by name
-    async getPlayerByName({ params }, res) {
-        const foundPlayer = await Player.findOne({ name: params.name });
-        if (!foundPlayer) {
-            return res.status(400).json({ message: 'Cannot find a player with this name!' });
-        }
-        res.json(foundPlayer);
-    },
+  async populateDB() {
+    const url =
+      "https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLPlayerList";
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": APIKEY,
+        "X-RapidAPI-Host":
+          "tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com",
+      },
+    };
 
-    // get players by position
-    async getPlayersByPosition({ params }, res) {
-        const foundPlayers = await Player.find({ position: params.position });
-        if (!foundPlayers) {
-            return res.status(400).json({ message: 'Cannot find players with this position!' });
-        }
-        res.json(foundPlayers);
-    },
-}
+    try {
+      const response = await fetch(url, options);
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  // get player(s) by name
+  async getPlayerByName({ params }, res) {
+    const foundPlayer = await Player.findOne({ name: params.name });
+    if (!foundPlayer) {
+      return res
+        .status(400)
+        .json({ message: "Cannot find a player with this name!" });
+    }
+    res.json(foundPlayer);
+  },
+
+  // get players by position
+  async getPlayersByPosition({ params }, res) {
+    const foundPlayers = await Player.find({ position: params.position });
+    if (!foundPlayers) {
+      return res
+        .status(400)
+        .json({ message: "Cannot find players with this position!" });
+    }
+    res.json(foundPlayers);
+  },
+};
