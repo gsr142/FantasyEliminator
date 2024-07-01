f1eaffea625678ca1ff3ac50543bad10be3be079
Greg Richardson <gsr142@gmail.com>
2024-07-01 13:36:36 -0700

    fix db schema so login and signup function properly

---
+++ client/src/components/LoginForm.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+import { loginUser } from "../utils/API";
+import Auth from "../utils/auth";
+const LoginForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError("");
+    setSuccess(false);
+
+    try {
+      const response = await loginUser(formData);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const { token, user } = await response.json();
+      console.log(user);
+      Auth.login(token);
+    } catch (err) {
+      console.error(error);
+    }
+  };
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ maxWidth: 400, mx: "auto", p: 2 }}
+    >
+
+      <TextField
+        label="Email"
+        name="email"
+        value={formData.email}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        required
+      />
+      <TextField
+        label="Password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        fullWidth
+        type="password"
+        margin="normal"
+        required
+      />
+      <Button
+        type="submit"
+        variant="contained"
+        color="primary"
+        fullWidth
+        disabled={loading}
+      >
+        {loading ? "Logging in..." : "Log in"}
+      </Button>
+      {success && (
+        <Box mt={2} color="success.main">
+          User logged in successfully!
+        </Box>
+      )}
+      {error && (
+        <Box mt={2} color="error.main">
+          {error}
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+export default LoginForm;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import SignupForm from "../components/SignupForm";
-
+import LoginForm from "../components/LoginForm";
 export default function Home() {
     return (
         <>
             <h1>Home</h1>
 
             <SignupForm />
+
+            <LoginForm />
         </>
     );
 }
